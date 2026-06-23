05a236b5a2030943db601fba834aa8232b21d59c
Bill Tam <bill.tam@btrust.ca>
2026-06-23 15:31:57 -0400

    test(api): register apikey_auth in dev_install so CI installs and collects it

--- openbb_platform/dev_install.py
+++ openbb_platform/dev_install.py
@@ -21,6 +21,7 @@ python = ">=3.10,<4"
 openbb-devtools = { path = "./extensions/devtools", develop = true, markers = "python_version >= '3.10'" }
 openbb-core = { path = "./core", develop = true }
 openbb-platform-api = { path = "./extensions/platform_api", develop = true }
+openbb-apikey-auth = { path = "./extensions/apikey_auth", develop = true }
 
 openbb-benzinga = { path = "./providers/benzinga", develop = true }
 openbb-bls = { path = "./providers/bls", develop = true }
